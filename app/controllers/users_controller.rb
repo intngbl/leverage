40670b844afda5b7e3dd3728517a6b0417015d8f
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-06-10 23:42:12 -0500

    Restrict access to user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    authorize! :index, @user, :message => t('cancan.failure.unauthorized')
     @users = User.all
   end
 
