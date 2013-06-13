acd9604049db12d2ce9021097d097f86bd80fce7
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-06-12 22:08:02 -0500

    Seed campaigns

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,14 @@ user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => EN
 user.confirm!
 puts 'user: ' << user.name
 user.add_role :admin
+agency = User.find_or_create_by_email :name => 'SEMANA', :email => 'fedecasas@gmail.com', :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+agency.confirm!
+puts 'user: ' << agency.name
+agency.add_role :agency
 
+puts 'DEFAULT CAMPAIGNS'
+agency.campaigns.delete_all
+virgin = agency.campaigns.create title: "Experience Virgin America", brief: "The Virgin America experience is like no other airline's.", price: "9.99"
+puts 'campaign: ' << virgin.title
+nike = agency.campaigns.create title: "Nike at the Olympics", brief: "Don't dream of winning. Train for it.", price: "4.99"
+puts 'campaign: ' << nike.title
