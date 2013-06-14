8a6b8bb95ff968513d1aae84aa1b1a9efbdfddee
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-06-14 15:59:11 -0500

    Fixes ActiveRecord::NotFound when accessing other's user campaign/edit
    All CampaignsController's actions use the provided user_id instead of
    current_user.id ... For some magical reason CanCan restrict access to
    users trying to make actions on another user's resources.
    
    We are covering this, with 2 specs (edit and delete crafted requests)

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,9 +1,9 @@
 class CampaignsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
 
-  load_resource :user, :only => [:index, :show]
-  load_resource :through => :user, :only => [:index, :show]
-  load_and_authorize_resource :through => :current_user, :only => [:new, :create, :edit, :update, :destroy]
+  load_resource :user
+  load_resource through: :user
+  authorize_resource except: [:index, :show]
 
   def create
     if @campaign.save
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -16,21 +16,29 @@ describe CampaignsController do
         post 'create', user_id: @user.id, campaign: { title: 'L', brief: 'O' } #, user: { role_ids: 1, confirmation_token: @user.confirmation_token }
       end
       it { response.should_not be_success }
-      it "should display error message" do
-        pending
-      end
     end
 
     describe "When user attempts to delete another's user campaign" do
       it "should not change the number of campaigns" do
-        # @other_user = FactoryGirl.create(:user)
-        # @other_campaign = FactoryGirl.create(:campaign, user: @other_user)
-        # params = { user_id: @other_user.id, id: @other_campaign.id }
-        # delete :destroy, params
-        # response.should_not be_success
-        # raises ActiveRecord::RecordNotFound
-        # since campaigns controller fetches campaign through current_user
-        pending
+        @other_user = FactoryGirl.create(:user)
+        @other_campaign = FactoryGirl.create(:campaign, user: @other_user)
+        params = { user_id: @other_user.id, id: @other_campaign.id }
+        delete :destroy, params
+        response.should_not be_success
+      end
+    end
+
+    describe "When user attempts to edit another's user campaign" do
+      before do
+        @other_user = FactoryGirl.create(:user)
+        @other_campaign = FactoryGirl.create(:campaign, user: @other_user)
+        @campaign_new_attributes = { title: 'Hacked', brief: 'by a n00b' }
+        put :update, { user_id: @other_user.id, id: @other_campaign.id, campaign: @campaign_new_attributes }
+      end
+
+      it { response.should_not be_success }
+      it "should not have changed the campaign's attribute" do
+        @other_campaign.title.should_not == @campaign_new_attributes[:title]
       end
     end
   end
