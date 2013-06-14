524be06fe5cfda410369a2af84fe19a9178cdbe4
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-06-14 10:38:45 -0500

    Refactor users_controller using load_and_authorize_resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
-  def index
-    authorize! :index, @user, :message => t('cancan.failure.unauthorized')
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
+  load_resource
+  authorize_resource :except => :show
 
   def update
-    authorize! :update, @user, :message => t('cancan.failure.unauthorized')
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user], :as => :admin)
       redirect_to users_path, :notice => "User updated."
     else
@@ -21,10 +13,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    authorize! :destroy, @user, :message => t('cancan.failure.unauthorized')
-    user = User.find(params[:id])
-    unless user == current_user
-      user.destroy
+    unless @user == current_user
+      @user.destroy
       redirect_to users_path, :notice => "User deleted."
     else
       redirect_to users_path, :notice => "Can't delete yourself."
