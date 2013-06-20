8b436009711fcc78f79531eaaf61e7123f368471
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-06-19 19:36:28 -0500

    Refactor EnrollmentsController by using load_and_authorize_resource

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,25 +1,20 @@
 class EnrollmentsController < ApplicationController
   before_filter :authenticate_user!
 
-  def create
-    authorize! :create, Enrollment
-    agency = User.find(params[:user_id])
-    campaign = agency.campaigns.find(params[:campaign_id])
+  load_and_authorize_resource
+  load_resource :user, instance_name: :agency
+  load_resource :campaign, through: :agency
 
-    if current_user.join!(campaign)
+  def create
+    if current_user.join!(@campaign)
       flash[:notice] = "You just joined the campaign."
     else
       flash[:error] = "Could not joined campaign."
     end
-    redirect_to user_campaign_path(agency, campaign)
+    redirect_to user_campaign_path(@agency, @campaign)
   end
 
   def destroy
-    authorize! :destroy, Enrollment
-
-    agency = User.find(params[:user_id])
-    campaign = agency.campaigns.find(params[:campaign_id])
-
     # Covers 2 cases:
     # 1 - when admin will unsubscribe users through the joined users table
     # 2 - when current_user unsubscribes himself from a campaign (leave button)
@@ -30,16 +25,16 @@ class EnrollmentsController < ApplicationController
       enrolled_user = current_user
     end
 
-    if enrolled_user.leave!(campaign)
+    if enrolled_user.leave!(@campaign)
       flash[:notice] = "Unsubscribed successfully."
     else
       flash[:error] = "Unsubscription failed."
     end
 
     if current_user.has_role? :tweeter
-      redirect_to user_campaign_path(agency, campaign)
+      redirect_to user_campaign_path(@agency, @campaign)
     else
-      redirect_to enrollment_user_campaign_path(agency, campaign)
+      redirect_to enrollment_user_campaign_path(@agency, @campaign)
     end
   end
 
