93d5596837c023ae5747e73961af4d60cb9e9402
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-07-11 20:41:21 -0500

    Add latest campaigns to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @search = Campaign.search(params[:q])
     @campaigns = @search.result
+    @latest_campaigns = Campaign.latest
   end
 end
