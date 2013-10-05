81f1a27c044a9b49604da99335ece08f781fee5d
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-10-05 17:48:19 -0500

    A table to manage tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,8 @@ class Tweet < ActiveRecord::Base
 
   validates :body, presence: true, length: { maximum: 140 }
   validates :campaign_id, presence: true
+
+  def sponsor
+    self.campaign.user
+  end
 end
