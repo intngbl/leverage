1b0dcb12bba27c233e625f80eb3f57143aeee52a
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-10-30 01:20:31 -0600

    Save users oauth tokens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130805015208) do
+ActiveRecord::Schema.define(:version => 20131030063941) do
 
   create_table "campaigns", :force => true do |t|
     t.string   "title"
@@ -119,6 +119,8 @@ ActiveRecord::Schema.define(:version => 20130805015208) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
